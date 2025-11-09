9422444baaae91b51745e43c6eba6c07efa1964e
luansantos333 <luangomespctu@gmail.com>
2025-11-09 09:43:16 -0300

    feat: send email when a new user is created

---
+++ mail-application/src/main/java/com/mailapplication/configuration/MailConfig.java
@@ -0,0 +1,40 @@
+package com.mailapplication.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${custom.host}")
+    private String host;
+    @Value("${custom.port}")
+    private int port;
+    @Value("${custom.username}")
+    private String username;
+    @Value("${custom.password}")
+    private String password;
+
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
--- mail-application/src/main/java/com/mailapplication/service/MailService.java
+++ mail-application/src/main/java/com/mailapplication/service/MailService.java
@@ -1,31 +1,40 @@
 package com.mailapplication.service;
 
 import com.mailapplication.dto.UserCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailService {
-    // @Autowired
-    //private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-    @KafkaListener (topics = "user-created", groupId = "user-created-group")
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @KafkaListener(topics = "user-created", groupId = "user-created-group")
     public UserCreatedEvent sendEmail(UserCreatedEvent userCreatedEvent) {
-        //  SimpleMailMessage mailMessage = new SimpleMailMessage();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
 
+        logger.info("Sending user-created email...");
 
-        System.out.printf("User created: %s%n", userCreatedEvent);
+        mailMessage.setSubject("Your new user has been successfully created");
+        mailMessage.setText("Hello, \n\n" + "We are happy to announce that your account has been successfully created. ");
+        mailMessage.setTo(userCreatedEvent.email());
+        mailSender.send(mailMessage);
 
-//        mailMessage.setSubject("Your new user has been successfully created");
-//        mailMessage.setText("Hello, \n\n" + "We are happy to announce that your account has been successfully created. ");
-//        mailMessage.setTo(userCreatedEvent.email());
-//        mailSender.send(mailMessage);
+        logger.info("Email sent successfully...");
 
         return userCreatedEvent;
 
     }
-
 
 
 }
