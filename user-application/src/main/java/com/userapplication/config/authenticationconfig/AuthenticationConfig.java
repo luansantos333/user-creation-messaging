a2382d3d7cac31e865827ccb04d23f2420868c24
luansantos333 <luangomespctu@gmail.com>
2025-11-14 20:22:18 -0300

    feat: add swagger documentation

--- user-application/src/main/java/com/userapplication/config/authenticationconfig/AuthenticationConfig.java
+++ user-application/src/main/java/com/userapplication/config/authenticationconfig/AuthenticationConfig.java
@@ -82,9 +82,13 @@ public class AuthenticationConfig {
     @Order(3)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((authorize) -> authorize.requestMatchers(HttpMethod.POST, "/api/user").permitAll().requestMatchers(HttpMethod.POST, "/api/user/reset/token/**").permitAll().requestMatchers("/actuator/**")
-                        .permitAll().
-                        anyRequest().authenticated())
+        http.authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers(HttpMethod.POST, "/api/user").permitAll().
+                                requestMatchers(HttpMethod.POST, "/api/user/reset/token/**").permitAll().requestMatchers("/actuator/**")
+                                .permitAll().requestMatchers("/v3/api-docs/**").permitAll().
+                                requestMatchers("/swagger-ui/**").permitAll().
+                                requestMatchers("swagger-ui.html").permitAll().
+                                anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(c -> c.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)).accessDeniedHandler((request, response, exception) -> {
